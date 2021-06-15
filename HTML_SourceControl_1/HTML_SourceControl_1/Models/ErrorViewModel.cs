290a01bddc075eff2ef36a18c885634db7779f89
Adam Miller <theadammiller56@gmail.com>
2021-06-15 09:42:49 -0500

    HTML VS Init

---
+++ HTML_SourceControl_1/HTML_SourceControl_1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTML_SourceControl_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
