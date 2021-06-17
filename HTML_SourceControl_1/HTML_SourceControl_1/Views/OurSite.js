a59e5f640120debd033f1342a06f84739b3e3f06
Conlan <cgotzion@gmail.com>
2021-06-17 11:29:26 -0500

    Show and Tell

---
+++ HTML_SourceControl_1/HTML_SourceControl_1/Views/OurSite.js
@@ -0,0 +1,9 @@
+﻿function showHide(input) {
+    var x = document.getElementById(input);
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
